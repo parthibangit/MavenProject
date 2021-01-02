a18fc397dfc67734050ea6479aad8f14d13bb45d
parthibangit <sarathipartha04@gmail.com>
2021-01-02 18:57:25 +0530

    New commit again

---
+++ src/test/java/testPackage/ConfigJenkin.java
@@ -0,0 +1,24 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class ConfigJenkin {
+    
+	@Test
+	public void seleniumNewTab() {		
+    
+	}
+	
+	@Test
+	public void seleniumLocators() {
+		
+	}
+	
+	@Test
+	public void seleniumScreenshot() {
+		
+	}
+	
+	
+
+}
